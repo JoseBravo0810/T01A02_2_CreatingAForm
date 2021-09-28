d0cd6056feabf9797036246ddb5c1caf10bab58e
Jose Bravo <jobravcas@gmail.com>
2021-09-28 08:44:05 +0200

    Añadir boton "Sign in"

--- src/t01a02_1_creatingaform/T01A02_1_CreatingAForm.java
+++ src/t01a02_1_creatingaform/T01A02_1_CreatingAForm.java
@@ -9,10 +9,12 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -43,6 +45,17 @@ public class T01A02_1_CreatingAForm extends Application {
         Text scenetitle = new Text("Welcome");
         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 0, 0, 2, 1);
+        
+        // Creacion del boton para recoger la accion
+        Button btn = new Button("Sign in");
+        // HBox coloca los nodos (componentes) en una fila horizontal, en lugar de en malla o rejilla _(grid)
+        HBox hbBtn = new HBox(10);
+        // Alineamos el boton abajo a la derecha
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        // Añadimos en el listado de hijos de hbBtn el boton
+        hbBtn.getChildren().add(btn);
+        // Añadimos el HBox en la primera columna y en la cuarta fila del casillero grid
+        grid.add(hbBtn, 1, 4);
         
         // Creamos la etiqueta para el nombre de usuario y la añadimos al gridPane
         Label userName = new Label("User Name:");
